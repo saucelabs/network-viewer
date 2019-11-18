2841a050ea33ab4ec68db4ae3fb49c41bd38756a
farhan-sauce <farhan.chauhan@saucelabs.com>
2019-11-18 16:40:25 +0530

    add loaded data example

--- examples/src/index.jsx
+++ examples/src/index.jsx
@@ -3,5 +3,8 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import NetworkViewer from './../../src/NetworkViewer';
+import NetworkData from './data/network.har';
 
-ReactDOM.render(<NetworkViewer />, document.getElementById('root'));
+const data = JSON.parse(NetworkData);
+
+ReactDOM.render(<NetworkViewer data={data} />, document.getElementById('root'));
