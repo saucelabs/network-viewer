275fd4f549f34c32f2e9ce767a106c675f70edca
farhan-sauce <farhan.chauhan@saucelabs.com>
2020-01-06 15:10:32 +0530

    implement tabs for req detail

---
+++ src/Components/Common/Tab.jsx
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types';
+
+const Tab = ({ children }) => children;
+
+Tab.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Tab;
---
+++ src/Components/Common/Tabs.jsx
@@ -0,0 +1,96 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+
+import Styles from './Tabs.styles.scss';
+
+const context = classNames.bind(Styles);
+
+const Tabs = ({
+  onUpdate,
+  selectedKey,
+  defaultSelectedKey,
+  children,
+  navTabsClassName,
+  tabsContainerClassName,
+  navLinkClassName,
+}) => {
+  const [items, updateItems] = useState([]);
+
+  useEffect(() => {
+    const itemsCollection = [];
+    React.Children.forEach(children, (element) => {
+      if (React.isValidElement(element)) {
+        const {
+          name,
+          eventKey: key,
+          children: component,
+        } = element.props;
+        itemsCollection.push({
+          name,
+          key,
+          component,
+        });
+      }
+    });
+    updateItems(itemsCollection);
+  }, []);
+
+  const [activeTab, updateTab] = useState(
+    defaultSelectedKey || (items && items.length ? items[0].key : null),
+  );
+  const handleUpdate = (key) => {
+    updateTab(key);
+    if (onUpdate) {
+      onUpdate(key);
+    }
+  };
+
+  const renderItem = () => {
+    const selectedTab = items.find(({ key }) => key === (selectedKey || activeTab));
+    return selectedTab ? selectedTab.component : null;
+  };
+
+  return (
+    <>
+      <nav className={context('nav-tabs', navTabsClassName)}>
+        {items.map(({ key: item }, index) => (
+          <a
+            key={item}
+            className={context('tab-item', navLinkClassName, { active: activeTab === item })}
+            onClick={() => handleUpdate(item)}
+            role="tab"
+            tabIndex={index}
+          >
+            {item}
+          </a>
+        ))}
+      </nav>
+      <section className={context(tabsContainerClassName)}>
+        {renderItem()}
+      </section>
+    </>
+  );
+};
+
+Tabs.propTypes = {
+  children: PropTypes.node,
+  defaultSelectedKey: PropTypes.string,
+  navLinkClassName: PropTypes.string,
+  navTabsClassName: PropTypes.string,
+  onUpdate: PropTypes.func,
+  selectedKey: PropTypes.string,
+  tabsContainerClassName: PropTypes.string,
+};
+
+Tabs.defaultProps = {
+  children: null,
+  defaultSelectedKey: null,
+  navLinkClassName: null,
+  navTabsClassName: null,
+  onUpdate: null,
+  selectedKey: null,
+  tabsContainerClassName: null,
+};
+
+export default Tabs;
