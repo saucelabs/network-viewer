97b15792b7175d7c64f10da7a5d1ad805cec9e1a
farhan-sauce <farhan.chauhan@saucelabs.com>
2020-07-05 22:22:24 +0530

    Updates

---
+++ precache-manifest.3032322c05c224f7fc07b0cd91eee5c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "785aac2f9454357b6b8b265c4e1a9bdf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "df5894cc0bd0c3e67a0b",
+    "url": "./static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "69f9e48fe3a2406615de",
+    "url": "./static/css/main.337f4fe7.chunk.css"
+  },
+  {
+    "revision": "df5894cc0bd0c3e67a0b",
+    "url": "./static/js/2.2ad08894.chunk.js"
+  },
+  {
+    "revision": "4721bde11a52b75eca36c3802b30af08",
+    "url": "./static/js/2.2ad08894.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69f9e48fe3a2406615de",
+    "url": "./static/js/main.54720788.chunk.js"
+  },
+  {
+    "revision": "773a59372dac63c69f32",
+    "url": "./static/js/runtime-main.2b2e16f1.js"
+  }
+]);
