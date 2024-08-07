c34d44a5cca80011891370cce007134c18b31f6e
Yu Yi Yang <yu-yi.yang@saucelabs.com>
2024-08-07 18:07:03 +0200

    remove tooltip

--- src/Components/NetworkTable/NetworkCellValue.jsx
+++ src/Components/NetworkTable/NetworkCellValue.jsx
@@ -4,8 +4,6 @@ import classNames from 'classnames/bind';
 
 import { formatValue } from '../../utils';
 import Styles from './NetworkTableHeader.styles.scss';
-import { VIEWER_FIELDS } from '../../constants';
-import Tooltip from '../Common/Tooltip/Tooltip';
 
 const context = classNames.bind(Styles);
 
@@ -15,42 +13,12 @@ const NetworkCellValue = ({
   payload,
 }) => {
   const formattedValue = formatValue(datakey, payload[datakey], unit, payload);
-  const shouldDisplayTooltip = (
-    datakey === VIEWER_FIELDS.file.key ||
-    payload.error
-  );
-
-  const getTitle = () => {
-    if (datakey === VIEWER_FIELDS.file.key) {
-      return payload.url;
-    }
-    if (payload.error) {
-      return payload.error;
-    }
-
-    return formattedValue;
-  };
-
-  if (!shouldDisplayTooltip) {
-    return (
-      <td className={context('value-cell', datakey)}>
-        <span className={Styles['value-text']}>
-          {formattedValue}
-        </span>
-      </td>
-    );
-  }
 
   return (
     <td className={context('value-cell', datakey)}>
-      <Tooltip
-        delay={500}
-        title={getTitle()}
-      >
-        <span className={Styles['value-text']}>
-          {formattedValue}
-        </span>
-      </Tooltip>
+      <span className={Styles['value-text']}>
+        {formattedValue}
+      </span>
     </td>
   );
 };
