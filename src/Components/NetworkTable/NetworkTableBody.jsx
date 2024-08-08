eb2c56a077e4db0fd348de963797bc39a046be83
Yu Yi Yang <yu-yi.yang@saucelabs.com>
2024-08-08 13:34:16 +0200

    remove unused deps

--- src/Components/NetworkTable/NetworkTableBody.jsx
+++ src/Components/NetworkTable/NetworkTableBody.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { FixedSizeGrid, FixedSizeList } from 'react-window';
-import AutoSizer from 'react-virtualized-auto-sizer';
 
 import { useNetwork } from '../../state/network/Context';
 import NetworkTableRow from './NetworkTableRow';
@@ -34,60 +32,6 @@ const NetworkTableBody = () => {
             scrollHighlight={selectedReqIndex === rowInfo.index}
           />
         ))}
-
-      {/* <AutoSizer> */}
-      {/*   {({ */}
-      {/*     height, */}
-      {/*     width, */}
-      {/*   }) => ( */}
-      {/*     <FixedSizeList */}
-      {/*       height={height} */}
-      {/*       itemCount={data.length} */}
-      {/*       itemData={data} */}
-      {/*       itemSize={32} */}
-      {/*       width={width} */}
-      {/*     > */}
-      {/*       {({ */}
-      {/*         entry, */}
-      {/*         index, */}
-      {/*         style, */}
-      {/*       }) => ( */}
-      {/*         <NetworkTableRow */}
-      {/*           key={index} */}
-      {/*           maxTime={totalNetworkTime} */}
-      {/*           onSelect={handleReqSelect} */}
-      {/*           payload={entry} */}
-      {/*           scrollHighlight={selectedReqIndex === index} */}
-      {/*           style={style} */}
-      {/*         /> */}
-      {/*       )} */}
-      {/*     </FixedSizeList> */}
-      {/*   )} */}
-      {/* </AutoSizer> */}
-
-      {/* <FixedSizeList */}
-      {/*   height={40} */}
-      {/*   itemCount={data.size} */}
-      {/*   itemData={data} */}
-      {/*   itemSize={32} */}
-      {/* > */}
-      {/*   {({ */}
-      {/*     entry, */}
-      {/*     index, */}
-      {/*   }) => ( */}
-      {/*     <span> */}
-      {/*       {`Hello ${index}`} */}
-      {/*     </span> */}
-      {/*     // <NetworkTableRow */}
-      {/*     //   key={index} */}
-      {/*     //   maxTime={totalNetworkTime} */}
-      {/*     //   onSelect={handleReqSelect} */}
-      {/*     //   payload={entry} */}
-      {/*     //   scrollHighlight={selectedReqIndex === index} */}
-      {/*     // /> */}
-      {/*   )} */}
-      {/* </FixedSizeList> */}
-      {/* {`Hello ${data.size}`} */}
       <div />
     </div>
   );
